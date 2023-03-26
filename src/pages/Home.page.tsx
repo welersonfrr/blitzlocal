5adc64babb64c7bf40a48e93e2a549af789414b0
Welersonfrr <contato.welerson@gmail.com>
2023-03-26 18:04:50 -0300

    Responsive to large screen

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -37,49 +37,54 @@ const Home = () => {
   };
 
   return (
-    <div className="h-full min-h-screen w-screen flex flex-col bg-gradient-to-br from-[#2B31A5] to-[#6265EB] gap-3">
-      {/* title/sub */}
-      <motion.div
-        initial={{ opacity: 0, x: 200 }}
-        animate={{ opacity: 1, x: 0 }}
-        className="m-6"
-      >
-        <h1 className="text-[3.5rem] font-bold text-white">
-          Veja se há Blitz próximo de você! 🚔🚦
-        </h1>
-        <p className="text-md text-white">
-          Monitore agora mesmo as blitz que estão acontecendo ao seu redor e não
-          seja mais pego desprevenido.
-        </p>
-      </motion.div>
-      {/* button */}
-      <motion.div
-        initial={{ opacity: 0, y: 200 }}
-        animate={{ opacity: 1, y: 0 }}
-        className="w-full mt-4 flex items-center justify-center drop-shadow-lg"
-      >
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-          className="p-4 rounded-md bg-[#6caaf9] text-white font-semibold"
-          onClick={obtainLocation}
+    <div className="h-screen min-h-screen w-screen flex flex-col bg-gradient-to-br from-[#2B31A5] to-[#6265EB] gap-3">
+      <div className="h-full w-full md:w-auto md:grid md:grid-cols-2">
+        {/* grid esq md */}
+        <div>
+          {/* title/sub */}
+          <motion.div
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            className="m-6"
+          >
+            <h1 className="text-[3.5rem] font-bold text-white">
+              Veja se há Blitz próximo de você! 🚔🚦
+            </h1>
+            <p className="text-md text-white">
+              Monitore agora mesmo as blitz que estão acontecendo ao seu redor e
+              não seja mais pego desprevenido.
+            </p>
+          </motion.div>
+          {/* button */}
+          <motion.div
+            initial={{ opacity: 0, y: 200 }}
+            animate={{ opacity: 1, y: 0 }}
+            className="w-full mt-4 flex items-center justify-center drop-shadow-lg"
+          >
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              className="p-4 rounded-md bg-[#6caaf9] text-white font-semibold"
+              onClick={obtainLocation}
+            >
+              Verificar se há blitz por perto
+            </motion.button>
+          </motion.div>
+        </div>
+        {/* map */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className="m-6 w-auto h-auto min-h-[300px] bg-white rounded-2xl border-2 border-[#6caaf9] overflow-hidden"
         >
-          Verificar se há blitz por perto
-        </motion.button>
-      </motion.div>
-      {/* map */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        className="m-6 w-auto h-auto min-h-[300px] bg-white rounded-2xl border-2 border-[#6caaf9] overflow-hidden"
-      >
-        <iframe
-          width="100%"
-          height="300px"
-          title="maps"
-          src="https://maps.google.com/maps?q=cascavel&t=&z=12&ie=UTF8&iwloc=&output=embed"
-        ></iframe>
-      </motion.div>
+          <iframe
+            width="100%"
+            height="100%"
+            title="maps"
+            src="https://maps.google.com/maps?q=cascavel&t=&z=12&ie=UTF8&iwloc=&output=embed"
+          />
+        </motion.div>
+      </div>
       {/* loading */}
       {loading && (
         <div className="absolute w-screen h-screen flex items-center justify-center backdrop-blur-sm bg-white/30">
